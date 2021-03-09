15f4f37a64baaf420742f6e9e74004e754c96566
Camilo Buitrago <camilobl1999@gmail.com>
2021-03-08 22:10:40 -0500

    pruebas de busqueda completadas completadas

---
+++ Toppick_App/lib/UserInterfaces/search_bar_button.dart
@@ -0,0 +1,49 @@
+//import 'package:Toppick_App/UserInterfaces/search_bar_full_screen.dart';
+import 'package:Toppick_App/UserInterfaces/search_bar_full_screen.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class SearchButton extends StatelessWidget {
+  String buttonText;
+  int option;
+  SearchButton(this.buttonText, this.option);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+      child: Center(
+        child: InkWell(
+          onTap: () => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => SearchScreen(this.option)),
+            )
+          },
+          child: Container(
+            height: 50.0,
+            width: 385.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Color(0xFFF3F3F3)),
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20.0),
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  buttonText,
+                  style: TextStyle(fontSize: 18.0, color: Color(0xFFC4C4C4)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- Toppick_App/lib/main.dart
+++ Toppick_App/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 import 'UserInterfaces/gradiant.dart';
+import 'UserInterfaces/search_bar_button.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,18 +16,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Toppick App',
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Stack(
-        children: [
-          Gradiant(),
-          Text(""),
-        ],
-      ),
-    );
+        title: 'Toppick App',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Stack(
+            children: [
+              Gradiant(),
+              Column(
+                children: [
+                  SearchButton("Buscar Tienda/Producto", 1),
+                  SearchButton("Buscar Tienda", 2),
+                  SearchButton("Buscar Producto", 3),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
