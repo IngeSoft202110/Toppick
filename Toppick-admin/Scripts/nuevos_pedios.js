d4b3612becff4f59ed4105e3510fd2c358e1ffcc
gabriela montejo <gabrielamcamacho@javeriana.edu.co>
2021-03-23 22:11:45 -0500

    Adición de scripts

---
+++ Toppick-admin/Scripts/nuevos_pedios.js
@@ -0,0 +1,37 @@
+'use strict'
+var plantilla = `
+<div class="pedidoNuevo">
+    <div class = "pedido" >
+        <div class = "parte">
+            <div class="columna" style="font-size: medium;"> 
+                <p style="justify-content: center;"><b> texto</b></p>
+            </div>
+            <div class="columna">
+                <button class="botonVerde">Aceptar </button>
+            </div>
+        </div>
+        <div class = "parte">
+            <div class="columna">
+                <button class="botonAzul">Ver detalles</button>
+            </div>
+            <div class="columna">
+                <button class="botonRojo">Rechazar </button>
+            </div>
+        </div>
+    </div>
+</div>
+`;
+function plantilla_nuevo_producto(){
+    var nuevoPedi = document.createDocumentFragment();
+    v
+
+}
+
+function nuevoPedido() {
+    var seccion = document.querySelector(".carta1");
+    var adicion = document.createTextNode(plantilla);
+    var x= document.createDocumentFragment(plantilla);
+    x.
+   // seccion.appendChild();
+    console.log(x);
+}
