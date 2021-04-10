91fdaef0f2191a9e5d6c971e6aec819de48a3406
JuanDiegoCamposNeira <juand.campos@javeriana.edu.co>
2021-04-10 03:12:24 -0500

    Se terminan las consultas en el servidor

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 /************************/
 const app = express(); 
 const PORT = process.env.PORT || 8001; 
-const CONNECTION_URL = ""; // Empty while the connection with MySQL is established
+
 
 /***************************/
 /* Data base configuration */
@@ -17,8 +17,11 @@ const CONNECTION_URL = ""; // Empty while the connection with MySQL is establish
 var connection = mysql.createConnection({
     host: 'localhost', 
     user: 'root', 
-    password: '1234567890' 
+    password: '1234567890', 
+    database: 'Toppick_Schema'
 }); 
+// Connect to database
+connection.connect( () => console.log("Connection with DB, succsessful") ); 
 
 
 /***************/
@@ -30,117 +33,133 @@ var connection = mysql.createConnection({
 /*****************/
 /* Web endpoints */
 /*****************/
+// Get all orders of the day (Historic)
+
+// Get a specific order info 
 app.get('/toppick/admin/:order_id', (req, res) => {
     // Url params
     const order_id = req.params.order_id; 
     // Data base query 
-    const query = ``; 
-    let querResult; 
-    // Connect to the DB 
-    connection.connect( () => console.log("Data base connected") ); 
+    const query = `SELECT * FROM Toppick_Schema.Pedido WHERE idPedido = ${order_id}`; 
     // Query DB 
     connection.query( query, (err, rows, fields) => {
         // Throw error if exists 
         if (err) throw err; 
-        // Save response 
-        querResult = rows; 
-        console.log("Query processed");
+        // Send response 
+        res.send(rows); 
     });
-    // Disconnect to tht DB 
-    connection.end( () => console.log("Connection with DB ended") ); 
-    // Send response
-    res.send(querResult); 
 }); 
 
 
 /*****************/
 /* App endpoints */
 /*****************/
-// Products from all University 
+// Products from all University ----> READY
 app.get('/toppick/app/products', (req, res) => {
     // Data base query 
-    const query = 'SHOW DATABASES'; 
-    let queryResult; 
-    // Connect to the DB 
-    connection.connect( () => console.log("Data base connected") ); 
+    const query = 'SELECT * FROM Toppick_Schema.Producto'; 
     // Query DB 
     connection.query( query, (err, rows, fields) => {
         // Throw error if exists 
         if (err) throw err; 
-        // Save response 
-        queryResult = rows; 
-        console.log("Query processed");
+        // Send response 
+        res.json(rows); 
     });
-    // Disconnect to tht DB 
-    connection.end( () => console.log("Connection with DB ended") ); 
-    // Send response 
-    res.send(queryResult); 
 }); 
 
-// Oppened stores in university
+// All oppened stores in university ----> READY
 app.get('/toppick/app/oppened-stores', (req, res) => {
     // Data base query 
-    const query = 'SHOW DATABASES'; 
-    let querResult; 
-    // Connect to the DB 
-    connection.connect( () => console.log("Data base connected") ); 
+    const query = "SELECT * FROM Toppick_Schema.PuntoDeVenta WHERE Estado = 'Abierto'"; 
     // Query DB 
     connection.query( query, (err, rows, fields) => {
         // Throw error if exists 
         if (err) throw err; 
-        // Save response 
-        querResult = rows; 
-        console.log("Query processed");
+        // Send response 
+        res.json(rows); 
     });
-    // Disconnect to tht DB 
-    connection.end( () => console.log("Connection with DB ended") ); 
-    // Send response
-    res.send(querResult); 
 }); 
 
-// Catalog from a store
-app.get('/toppick/app/catalog/:store_name', (req, res) => {
-    const store_name = req.params.store_name; 
+// Catalog from a given store ----> READY
+app.get('/toppick/app/catalog/:store_id', (req, res) => {
+    const store_id = req.params.store_id; 
     // Data base query 
-    const query = ``; 
-    let querResult; 
-    // Connect to the DB 
-    connection.connect( () => console.log("Data base connected") ); 
+    const query = 'SELECT idProducto, nombreProducto, precio, tiempoPreparacion, Producto.calificacion, Producto.urlImagen, categoria '
+                + 'FROM toppick_schema.producto, toppick_schema.puntodeventa, toppick_schema.catalogo ' 
+                + `WHERE idPuntoDeVenta = ${store_id} and PuntodeVenta_idPuntodeVenta = idPuntodeventa and Producto_idProducto = idProducto`; 
     // Query DB 
     connection.query( query, (err, rows, fields) => {
         // Throw error if exists 
         if (err) throw err; 
-        // Save response 
-        querResult = rows; 
-        console.log("Query processed");
+        // Send response 
+        res.json(rows); 
     });
-    // Disconnect to tht DB 
-    connection.end( () => console.log("Connection with DB ended") ); 
-    // Send response
-    res.send(querResult);  
 }); 
 
-// Store that have a product in his catalog 
+// Store that have a product in his catalog ------> Missing query
 app.get('/toppick/app/:product_name/in/:store_name', (req, res) => {
     const store_name = req.params.store_name; 
     const product_name = req.params.product_name; 
     // Data base query 
     const query = ``; 
-    let querResult; 
-    // Connect to the DB 
-    connection.connect( () => console.log("Data base connected") ); 
     // Query DB 
     connection.query( query, (err, rows, fields) => {
         // Throw error if exists 
         if (err) throw err; 
-        // Save response 
-        querResult = rows; 
-        console.log("Query processed");
+        // Send response
+        res.json(rows); 
     });
-    // Disconnect to tht DB 
-    connection.end( () => console.log("Connection with DB ended") ); 
-    // Send response
-    res.send(querResult); 
+}); 
+
+// Query #1 -----> Missing endoint
+app.get('', (req, res) => {
+    // Url params
+    const product_id = req.params.product_id; 
+    // Data base query 
+    const query = 'SELECT idPuntodeVenta, Usuario_nombreUsuario, Usuario_contraseña, nombrePuntoDeVenta, tipoPuntoVenta, urlUbicacion, Estado, PuntoDeVenta.urlImagen '
+                + 'FROM toppick_schema.producto, toppick_schema.puntodeventa, toppick_schema.catalogo '
+                + `WHERE idProducto = ${product_id} and PuntodeVenta_idPuntodeVenta = idPuntodeventa and Producto_idProducto = idProducto;`; 
+    // Query DB 
+    connection.query( query, (err, rows, fields) => {
+        // Throw error if exists 
+        if (err) throw err; 
+        // Send response
+        res.json(rows); 
+    });
+}); 
+
+// Query #2 ------> Missing endpoint
+app.get('', (req, res) => {
+    // Url params
+    const product_id = req.params.product_id; 
+    // Data base query 
+    const query = 'SELECT idAcompañamiento, nombreAcompañamiento, tipo '
+                + 'FROM toppick_schema.Acompañamiento, toppick_schema.AcompañamientoXEspecialidad, toppick_schema.especialidad, toppick_schema.producto '
+                + `WHERE idProducto = ${product_id} and Producto_IdProducto = idProducto and Especialidad_Producto_idProducto = idProducto and idacompañamiento = acompañamiento_idacompañamiento`; 
+    // Query DB 
+    connection.query( query, (err, rows, fields) => {
+        // Throw error if exists 
+        if (err) throw err; 
+        // Send response
+        res.json(rows); 
+    });
+}); 
+
+// Query #3 ------> Missing endpoint
+app.get('', (req, res) => {
+    // Url params 
+    const product_id = req.params.product_id; 
+    // Data base query 
+    const query = 'SELECT idProducto, nombreProducto, precio, tiempoPreparacion, calificacion, urlImagen, categoria '
+                + 'from toppick_schema.Combo, toppick_schema.ProductoXCombo, toppick_schema.producto '
+                + `where toppick_schema.Combo.Producto_idProducto = ${product_id} and toppick_schema.ProductoXCombo.Producto_IdProducto = idProducto and toppick_schema.ProductoXCombo.Combo_Producto_idProducto = toppick_schema.Combo.Producto_idProducto`; 
+    // Query DB 
+    connection.query( query, (err, rows, fields) => {
+        // Throw error if exists 
+        if (err) throw err; 
+        // Send response
+        res.json(rows); 
+    });
 }); 
 
 
